f27439578ad809515cdddee232c9c5c0cf0c8e1d
owlai01 <152986905+owlai01@users.noreply.github.com>
2024-03-24 12:45:12 +0300

    Add files via upload

--- commands/edit/quotely.js
+++ commands/edit/quotely.js
@@ -10,7 +10,7 @@ const quotelyY = async (client, text, m, pushname) => {
     try {
         const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
         const imageBuffer = Buffer.from(response.data, 'binary');
-        await client.sendImageAsSticker(m.chat, imageBuffer, m, { packname: pushname, author: "dreaded" });
+        await client.sendImageAsSticker(m.chat, imageBuffer, m, { packname: pushname, author: "𓅓Owl" });
     } catch (error) {
         console.error('Error fetching image:', error);
     }
